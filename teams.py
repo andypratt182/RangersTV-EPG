77e0f3a4548dc3a81e85f6d49d4b9a131aaaab7d
andypratt182 <63167693+andypratt182@users.noreply.github.com>
2026-08-07 12:34:04 +0100

    Update teams.py

--- teams.py
+++ teams.py
@@ -3,61 +3,73 @@ SPFL_TEAMS = {
     "rangerstv": {
         "name": "Rangers TV",
         "urn": "urn:bbc:sportsdata:football:team:rangers",
+        "stadium": "Ibrox Stadium",
     },
 
     "celtictv": {
         "name": "Celtic TV",
         "urn": "urn:bbc:sportsdata:football:team:celtic",
+        "stadium": "Celtic Park",
     },
 
     "aberdeentv": {
         "name": "Aberdeen TV",
         "urn": "urn:bbc:sportsdata:football:team:aberdeen",
+        "stadium": "Pittodrie Stadium",
     },
 
     "dundeetv": {
         "name": "Dundee TV",
         "urn": "urn:bbc:sportsdata:football:team:dundee",
+        "stadium": "Dens Park",
     },
 
     "dundeeunitedtv": {
         "name": "Dundee United TV",
         "urn": "urn:bbc:sportsdata:football:team:dundee-united",
+        "stadium": "Tannadice Park",
     },
 
     "heartstv": {
         "name": "Hearts TV",
         "urn": "urn:bbc:sportsdata:football:team:heart-of-midlothian",
+        "stadium": "Tynecastle Park",
     },
 
     "hibstv": {
         "name": "Hibernian TV",
         "urn": "urn:bbc:sportsdata:football:team:hibernian",
+        "stadium": "Easter Road",
     },
 
     "kilmarnocktv": {
         "name": "Kilmarnock TV",
         "urn": "urn:bbc:sportsdata:football:team:kilmarnock",
+        "stadium": "Rugby Park",
     },
 
     "motherwelltv": {
         "name": "Motherwell TV",
         "urn": "urn:bbc:sportsdata:football:team:motherwell",
+        "stadium": "Fir Park",
     },
 
     "falkirktv": {
         "name": "Falkirk TV",
         "urn": "urn:bbc:sportsdata:football:team:falkirk",
+        "stadium": "Falkirk Stadium",
     },
 
     "livingstontv": {
         "name": "Livingston TV",
         "urn": "urn:bbc:sportsdata:football:team:livingston",
+        "stadium": "Almondvale Stadium",
     },
 
     "stmirrentv": {
         "name": "St Mirren TV",
         "urn": "urn:bbc:sportsdata:football:team:st-mirren",
+        "stadium": "The SMiSA Stadium",
     },
 
 }
