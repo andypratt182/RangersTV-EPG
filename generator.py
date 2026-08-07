b2c9694909e42b78360adaec0b118644d3e5ec2d
andypratt182 <63167693+andypratt182@users.noreply.github.com>
2026-08-07 02:50:09 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,12 @@
+from fixtures import get_fixtures
+from xmltv import create_xmltv
 
+if __name__ == "__main__":
+    fixtures = get_fixtures()
+
+    create_xmltv(
+        fixtures,
+        "output/rangerstv.xml"
+    )
+
+    print("Rangers TV EPG generated successfully")
