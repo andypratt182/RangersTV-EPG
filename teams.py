7556d29b72f78973f86babb6ba95dbd29c2c096f
andypratt182 <63167693+andypratt182@users.noreply.github.com>
2026-08-07 11:52:57 +0100

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,63 @@
+SPFL_TEAMS = {
+
+    "rangerstv": {
+        "name": "Rangers TV",
+        "urn": "urn:bbc:sportsdata:football:team:rangers",
+    },
+
+    "celtictv": {
+        "name": "Celtic TV",
+        "urn": "urn:bbc:sportsdata:football:team:celtic",
+    },
+
+    "aberdeentv": {
+        "name": "Aberdeen TV",
+        "urn": "urn:bbc:sportsdata:football:team:aberdeen",
+    },
+
+    "dundeetv": {
+        "name": "Dundee TV",
+        "urn": "urn:bbc:sportsdata:football:team:dundee",
+    },
+
+    "dundeeunitedtv": {
+        "name": "Dundee United TV",
+        "urn": "urn:bbc:sportsdata:football:team:dundee-united",
+    },
+
+    "heartstv": {
+        "name": "Hearts TV",
+        "urn": "urn:bbc:sportsdata:football:team:hearts",
+    },
+
+    "hibstv": {
+        "name": "Hibernian TV",
+        "urn": "urn:bbc:sportsdata:football:team:hibernian",
+    },
+
+    "kilmarnocktv": {
+        "name": "Kilmarnock TV",
+        "urn": "urn:bbc:sportsdata:football:team:kilmarnock",
+    },
+
+    "motherwelltv": {
+        "name": "Motherwell TV",
+        "urn": "urn:bbc:sportsdata:football:team:motherwell",
+    },
+
+    "rosscountytv": {
+        "name": "Ross County TV",
+        "urn": "urn:bbc:sportsdata:football:team:ross-county",
+    },
+
+    "stjohnstonetv": {
+        "name": "St Johnstone TV",
+        "urn": "urn:bbc:sportsdata:football:team:st-johnstone",
+    },
+
+    "stmirrentv": {
+        "name": "St Mirren TV",
+        "urn": "urn:bbc:sportsdata:football:team:st-mirren",
+    },
+
+}
